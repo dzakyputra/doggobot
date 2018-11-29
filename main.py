ff01c03bfbeb751e4a95b89faf2c68c10247f31c
dzakyputra <dzakok@gmail.com>
2018-11-29 10:40:00 +0700

    first_commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def get_image_url():
+    allowed_extension = ['png','jpg']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+        print(file_extension)
+    return url
+
+def bop(bot, update):
+    url = get_image_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('704418931:AAEtcZ7gsapkxzBliCOR9AsgR7f8N_AVQHA')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
